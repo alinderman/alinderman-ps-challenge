5d2712ac32a04ed337c457ade4c6dc123c4901fa
Adam <alinderman@gocatalant.com>
2022-02-13 14:36:29 -0500

    Platform Science Coding Challenge

---
+++ run.js
@@ -0,0 +1,127 @@
+/**
+ * @module run.js - Calculates the optimal score of matches between
+ * addresses and delivery drivers based on a proprietary algorithm
+ */
+
+const fs = require('fs');
+const process = require('process')
+const { primeFactors } = require('./utils/math.js')
+
+// Regular expressios to extract street name from expected line input
+const ADDRESS_RE = /^[\w]+\s([\w\s]+)\s[\w]+\,/i
+// Regular expression to count vowels in driver name
+const VOWEL_RE = /[aeiou]/ig
+// Regular expression to count consonants in driver name
+const CONSONANT_RE = /[bcdfghjklmnpqrstvwxyz]/ig
+// List indices
+ADDRESS_INDEX = 0
+ADDRESS_FLAG_INDEX = 1
+DRIVER_NAME_INDEX = 0
+DRIVER_VOWEL_COUNT_INDEX = 1
+DRIVER_CONSONANT_COUNT_INDEX = 2
+DRIVER_FACTOR_INDEX = 3
+DRIVER_FLAG_INDEX = 4
+SCORE_INDEX = 0
+SCORE_ADDRESS_INDEX = 1
+SCORE_DRIVER_INDEX = 2
+
+
+// Read input files
+const addressFile = fs.readFileSync(process.argv[2], 'utf-8')
+const driverFile = fs.readFileSync(process.argv[3], 'utf-8')
+
+/*
+ * Street names of addresses parsed from the input document.
+ * 
+ * Array of Arrays, one for each address, with the following
+ * information for each:
+ *    - {string} The street name
+ *    - {boolean} Flag for determining when an optimal match
+ *        has been determined for this address
+ */
+const addresses = []
+
+
+/*
+ * Full names of available delivery drivers
+ * 
+ * Array of Arrays, one for each driver, with the following
+ * information for each:
+ *    - {string} The full name
+ *    - {number} The # of vowels in the name
+ *    - {number} The # of consonants in the name
+ *    - {Set} The prime # factors of the length of the driver's name
+ *    - {boolean} Flag for determining when an optimal match
+ *        has been determined for this driver
+ */
+const drivers = []
+
+
+/*
+ * The algorithmically calculated scores for each combination
+ * of address and driver
+ * 
+ * Array of Arrays, one for each combination, with the following
+ * information for each:
+ *    - {number} The score for the combination
+ *    - {number} The index of the address in the list of addresses
+ *    - {number} The index of the driver in the list of drivers
+ */
+const scores = []
+
+// Extract information from each document
+addressFile.split(/\r?\n/).forEach(line =>  {
+  addresses.push([line.match(ADDRESS_RE)[1], false])
+});
+
+driverFile.split(/\r?\n/).forEach(line =>  {
+  let consonants = line.match(CONSONANT_RE)
+  let vowels = line.match(VOWEL_RE)
+
+  drivers.push([line, vowels.length, consonants.length, primeFactors(line.length), false])
+});
+
+// For each combination of driver and address, calculate the score
+addresses.forEach((address, aIndex) => {
+	let aFactors = [...primeFactors(address[ADDRESS_INDEX].length)]
+	let aScores = []
+
+	let even = address[ADDRESS_INDEX].length % 2 == 0
+	drivers.forEach((driver, dIndex) => {
+		let score = 0
+		if (even) {
+			score = driver[DRIVER_VOWEL_COUNT_INDEX] * 1.5
+		} else {
+			score = driver[DRIVER_CONSONANT_COUNT_INDEX]
+		}
+		if (aFactors.some(f => driver[DRIVER_FACTOR_INDEX].has(f))) {
+			score = score * 1.5
+		}
+		scores.push([score, aIndex, dIndex])
+	})
+})
+
+// Sort the entire list of scores from high to low
+scores.sort((score1, score2) => {
+	return score2[SCORE_INDEX] - score1[SCORE_INDEX]
+})
+
+// Iterate down the list of scores until all addresses have a driver.
+// Starting from the highest score and working our way through the list
+// until we find a unique combination for each address ensures we get 
+// the highest possible score.
+let assignments = []
+scores.forEach(score => {
+	const addressIndex = score[SCORE_ADDRESS_INDEX], driverIndex = score[SCORE_DRIVER_INDEX]
+	const address = addresses[addressIndex], driver = drivers[driverIndex]
+	const addressReady = !address[ADDRESS_FLAG_INDEX], 
+		driverReady = !driver[DRIVER_FLAG_INDEX]
+
+	if (addressReady && driverReady) {
+		address[ADDRESS_FLAG_INDEX] = true
+		driver[DRIVER_FLAG_INDEX] = true
+		assignments.push([addressIndex, driverIndex])
+		console.log(`Assigning ${driver[DRIVER_NAME_INDEX]} to ${address[ADDRESS_INDEX]}: ${score[SCORE_INDEX]}`)
+	}
+})
+
---
+++ utils/math.js
@@ -0,0 +1,121 @@
+/**
+ * @module math.js
+ */
+
+/* Internal state */
+
+/* Memoize any previously verified prime numbers */
+const knownPrimes = new Set()
+
+// Our naive method of checking possible prime numbers can't tell us about 2 or 3
+knownPrimes.add(2)
+knownPrimes.add(3)
+
+/* Track the highest prime number we have yet encountered */
+let maxPrime = 0
+
+
+/**
+ * Locates all unique prime factors for an input
+ * 
+ * Based on the principal that all prime numbers are of the form 6n + 1 or 6n - 1. This gives us a concrete
+ * set of numbers to check (besides all of them), but only tells us if a number _might_ be prime.
+ * By recursively checking each possible prime to see if it also has a prime factor, we can validate. 
+ * From there we can make note of each we come across to avoid repeating calculations.
+ * 
+ * @arg {number} num - the number to generate factors for
+ * @arg {boolean} [first] - whether to return when the first factor is identified; used in recursive calls to 
+ *     short-circuit as it's only necessary to know if the input itself is prime
+ * 
+ * @returns {Set} the unique set of prime factors of the input
+ */
+function primeFactors(num, first = false) {
+	const factors = new Set()
+
+	// We don't want to include 1 as every number will have it.
+	if (num == 1) {
+		return factors
+	}
+
+	// Check against 2 and 3 as those can't be calculated
+	if (num % 2 == 0) {
+		factors.add(2)
+		if (first) {
+			return factors
+		}
+	}
+
+	if (num % 3 == 0) {
+		factors.add(3)
+		if (first) {
+			return factors
+		}
+	}
+
+	// Starting at 5, calculate and verify any possible primes that are lower than the input
+	if (num > 4) {
+		let base = 0,
+			pivot = 0,
+			primeLow = 0,
+			primeHigh = 0
+
+		do {
+			// For every integer n, there is a possible prime at 6n +/-
+			base++
+			pivot = 6 * base
+			primeLow = pivot - 1
+			primeHigh = pivot + 1
+
+			const checkPrime = (primeCandidate) => {
+				// A possible prime must be prime if we haven't already encountered a factor
+				let isDefinitelyPrime = factors.size == 0 && num == primeCandidate
+
+				if (!isDefinitelyPrime) {
+					// This is a possible prime, but it's not equal to the input. Recursively check
+					// if this possible prime has any prime factors (besides itself)
+					let secondaryFactors = primeFactors(primeCandidate, true)
+
+					// If we got only one factor back, and it's the possible one we passed in, we know for sure
+					isDefinitelyPrime = secondaryFactors.size == 1 && secondaryFactors[1] == primeCandidate
+				}
+
+				if (isDefinitelyPrime) {
+					// Remember so we don't check it again and set the bar for the next check 
+					knownPrimes.add(primeCandidate)
+					if (primeCandidate > maxPrime) {
+						maxPrime = num
+					}
+				}
+			}
+
+			// If this is a prime number we haven't checked before, validate whether it is prime
+			if (primeLow > maxPrime) {
+				checkPrime(primeLow)
+			}
+			if (primeHigh > maxPrime) {
+				checkPrime(primeHigh)
+			}
+
+			// Now we can be sure if one of those 2 candidates are prime, they are in knownPrimes, so check if
+			// either one is a factor of the input
+			if (num % primeLow == 0 && knownPrimes.has(primeLow)) {
+				factors.add(primeLow)
+				if (first) {
+					return factors
+				}
+			}
+			if (num % primeHigh == 0 && knownPrimes.has(primeHigh)) {
+				factors.add(primeHigh)
+				if (first) {
+					return factors
+				}
+			}
+		} while (primeHigh < num)
+	}
+
+	return factors
+}
+
+module.exports = {
+	primeFactors
+}
